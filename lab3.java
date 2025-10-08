0c47f971a5416e33f5546fc3f379ba2964e95b0f
medha-s19 <medhasrinathbsc24@rvu.edu.in>
2025-10-09 00:25:45 +0530

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -1,26 +1,26 @@
 class Vehicle {
-void show() {
-System.out.println("This is a vehicle");
-}
+    void show() {
+        System.out.println("This is a vehicle");
+    }
 }
 
 class Car extends Vehicle {
-void show() {
-super.show();
-System.out.println("This is a car");
-}
+    void show() {
+        super.show();
+        System.out.println("This is a car");
+    }
 }
 
 class ElectricCar extends Car {
-void show() {
-super.show();
-System.out.println("This is an electric car");
-}
+    void show() {
+        super.show();
+        System.out.println("This is an electric car");
+    }
 }
 
 public class lab3 {
-public static void main(String[] args) {
-ElectricCar e = new ElectricCar();
-e.show();
-}
+    public static void main(String[] args) {
+        ElectricCar e = new ElectricCar();
+        e.show();
+    }
 }
